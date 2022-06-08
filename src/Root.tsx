b0f73d90575406fce4f783d594e37ad7fdb222a0
thkim7527 <thkim7527@gmail.com>
2022-06-08 15:44:09 +0900

    Fix file names

--- src/Root.tsx
+++ src/Root.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 
 import App from "./components/App";
 
-import "./style/style.css";
+import "./style/Root.css";
 
 ReactDOM
     .createRoot(document.querySelector("#root")!)
